00f992782f4ba42e7aa719bd6aaaccbd77503da2
shailesh salekar <shailesh.salekar@accionlabs.com>
2021-02-09 20:52:19 +0530

    variables and data types

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
-console.log('Hello world')
+// variables
+
+// var a = 10;
+
+// var a; //declaration
+// a = 20; //assignment
+
+
+// var a = 10;
+// var b  = 20;
+
+// var c = a+b;
+// console.log(c);
+
+
+// data types
+// 1. Number
+// 2. String
+// 3. Boolean
+
+//number data type
+// var a = 10.234
+// var b = 10
+// var c = 0.2222
+// var d = -34
+
+// //string data type
+
+// var name = "shailesh"
+// var lastName = 'salekar'
+
+// // boolean data type
+
+// var day = false;//0
+// var night = true;//1
+
+
+var a = 40;
+var b = 70;
+var c = "50";
+
+console.log(c+b+a)
+
+var firstName = "shailesh";
+var lastName = "Salekar";
+
+console.log(firstName+" "+lastName)
+
+
+var night = true;
+
+// console.log(!night)
+
+// keywords
+
+var marks = 0;
+
+
+if(marks){
+    console.log(marks)
+}
+
+
+
+// 110 +"50" = string = 11050
+// concetination
+
+
