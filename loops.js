88ecb35c20aa0ed07290d44ad05c9950f3b56700
shailesh <shailesh@Administrators-MacBook-Pro.local>
2021-02-27 20:46:57 +0530

    If statment and for loops

---
+++ loops.js
@@ -0,0 +1,49 @@
+
+
+// 1 to 10;
+
+
+
+// for(declaration; condition; increment/decrement){
+
+// }
+
+
+var sum = 0;
+// for(var i = 1;i<=10;i++){
+//     if(i == 3 || i ==6){
+//         continue; //skip   
+//     }else{
+//         sum  = sum+i
+//     }
+// }
+
+
+var sum = 0;
+for(var i = 1;i<=10;i++){
+    if(i == 3){
+        break;
+    }else{
+        sum = sum+i
+    }
+}
+
+console.log(sum)
+
+
+
+// i = 1, sum = 0
+// sum = 1
+// i=2, sum = 1
+// sum = 3
+// i=3, sum=3
+//sum = 6
+
+
+
+// i
+// 1,2,3,4,5,6,7,8,9,10
+
+// when i  = 3 and i = 6
+
+
---
+++ statements.js
@@ -0,0 +1,80 @@
+// Operators
+
+// arethmetic operators         +,-,/,%,*
+// logical                      ==,===,>,<,>=,<=, !, &&, ||
+// assignment                   =,=+,=-
+
+// if(condition //true){
+//         //this line will execute
+// }else{
+//     //else part will execute if condition is false
+// }
+
+
+
+// if(condition){
+//     //1
+// }else if(condition2){
+//     //2
+// }else{
+//     //default
+// }
+
+
+
+
+// let left = 10;
+// let right = 20;
+
+
+// console.log(left && right)
+
+
+
+// A           B                   result
+
+// true        true                true
+// true        false              true
+// false       true                true
+// false       false               false
+
+
+
+
+// marks greater than equal to 35 and less than 59 - passed
+//marks greater than 59 and less than equal to 74  - first class
+// marks greater than 75 distinction
+//marks less than 35 failed
+
+// let marks = 74;
+
+
+// if( marks >=35 && marks <=59){
+//     console.log("Passed")
+// }else if(marks >59 && marks <=74){
+//     console.log("First Class")
+// }else if(marks>74 && marks <=100){
+//     console.log("Distinction")
+// }else if(marks<35 && marks >=0){
+//     console.log("Failed")
+// }
+
+
+
+
+let day = "Monday";
+let weekend = false;
+
+if(day == "Saturday" || day == "Sunday"){
+    weekend = true
+}else{
+    weekend = false;
+}
+
+
+console.log(weekend)
+
+
+
+
+
